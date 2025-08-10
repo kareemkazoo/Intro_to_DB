1d53ed2a52577257df6d52426b12758c4c623e52
KareemKazoo <kimo19950425@gmail.com>
2025-08-11 02:40:55 +0300

    Fixed

--- task_2.sql
+++ task_2.sql
@@ -28,11 +28,12 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_date DATE,
 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id));
 
-CREATE TABLE IF NOT EXISTS Order_details (
+
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
